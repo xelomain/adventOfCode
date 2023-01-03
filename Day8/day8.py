267db00266f00d12ae23ae49bb6f98d2f6a515de
Ole Nies <ole.nies@actiware.de>
2023-01-03 10:28:29 +0100

    add all

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def readInput():
     input = open("puzzleInput8.txt", "r")
     readFile = input.read()
@@ -30,37 +30,5 @@ def dayEight():
     return total
     
 ergebnis = dayEight()
-=======
-def readInput():
-    input = open("puzzleInput8.txt", "r")
-    readFile = input.read()
-    newInput = readFile.split('\n')
-    return newInput
 
-def dayEight():
-
-    input = readInput()
-
-    rows = len(input)
-    columns = len(input[0])
-
-    edges = (rows*2) + ((columns-2)*2)
-    total = edges
-
-    for row in range(1, rows-1):
-        for col in range(1, columns-1):
-            tree = input[row][col]
-
-            left  = [input[row][col-i] for i in range(1, col+1)]
-            right = [input[row][col+i] for i in range(1, columns-col)]
-            up =  [input[row-i][col] for i in range(1, row+1)]
-            down = [input[row+i][col] for i in range(1, rows-row)]
-
-            if max(left) < tree or max(right) < tree or max(up) < tree or max(down) < tree:
-                total += 1
-
-    return total
-    
-ergebnis = dayEight()
->>>>>>> eacf3d7f71565743860fc17120ec0204caf3846d
 print(ergebnis)
